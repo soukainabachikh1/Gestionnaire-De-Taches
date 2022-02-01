98659269f14ff6b6cdb254953ff4d2f360cad39c
soukainabachikh1 <92301501+soukainabachikh1@users.noreply.github.com>
2022-02-01 13:06:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 
 
-// Show Nav Bar
+
 
 const nav = document.querySelector('.nav-menu');
 const toggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav-link');
+const closeToggle = document.querySelectorAll('.close');
+const linkColor = document.querySelectorAll('.nav-link');
 
+// Show Nav Bar
 toggle.onclick = function () {
     nav.classList.toggle('show-nav')
 }
 
 // Remove Menu Bar
 
-const navLink = document.querySelectorAll('.nav-link');
-const closeToggle = document.querySelectorAll('.close');
 
 function linkAction() {
     const navMenu = document.querySelector('.nav-menu');
@@ -23,7 +25,7 @@ closeToggle.forEach(n => n.addEventListener('click', linkAction));
 
 // Active Link 
 
-const linkColor = document.querySelectorAll('.nav-link');
+
 function colorLink() {
     if (linkColor) {
         linkColor.forEach(L => L.classList.remove('active'))
